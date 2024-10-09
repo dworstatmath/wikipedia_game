de5cfc738b5a51e46b56b40b740fad88caceb348
dworstatmath <ian.alexander.ng@gmail.com>
2024-10-09 17:18:59 +0000

    2 steps

--- wiki.py
+++ wiki.py
@@ -1,6 +1,6 @@
 import wikipediaapi
 import time
-
+from queue import Queue
 user_agent="wikipedia_game (ian.alexander.ng@gmail.com)"
 wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
@@ -14,17 +14,47 @@ def fetch_links(page):
     return links_list
 
 def wikipedia_game_solver(start_page, target_page):
-    links = fetch_links(start_page)
-    for link in links:
-        if link==target_page.title:
-            return(True)
-    return(False)
+    print("working on it.....")
+    start_time=time.time()
+    
+    queue=Queue() #queue for when items to check next.
+    visited=set() #keeps track of visited links
+    parent={} #dictionary ti eep track of parent.
+    # start off by adding the start page to our queu and visited
+    queue.put(start_page.title)
+    visited.add(start_page.title)
+    #keep looping as long as queu is not empty
+    while not queue.empty():
+        #get next item in our queue
+        current_page_title =queue.get()
+        if current_page_title == target_page.title:
+            break
+        current_page=wiki.page(current_page_title)
+        links =fetch_links(current_page)
 
+        #gothrough ech of the links and add them to the queue
+        for link in links:
+            ##if page hasn't already been visited
+            if  link not in visited:
+                queue.put(link)
+                visited.add(link)
+                parent[link]= current_page_title
+    #reconstruct path from target page ti start oage
+    path =[]
+    page_title= target_page.title
+    while page_title != start_page.title:
+        path.append(page_title)
+        page_title=parent[page_title]
+    path.append(start_page.title)
+    path.reverse()
 
+    end_time=time.time()
+    print("this algorithm took",  end_time-start_time, "seconds")
+    return path
 start_page = wiki.page("Ring (mathematics)")
-target_page = wiki.page("Field (mathematics)")
+target_page=wiki.page("Ostrowski's theorem")
 
 print(start_page.links)
 
 print(fetch_links(start_page))
-print(wikipedia_game_solver(start_page,target_page))
+print(wikipedia_game_solver(start_page,target_page)) 
